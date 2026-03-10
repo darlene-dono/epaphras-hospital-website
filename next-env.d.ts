dbea4dc44f9a659936f810d22f1f0e4273c79db0
Emmanuel <140660538+AE4041@users.noreply.github.com>
2026-03-10 01:16:44 +0000

    Updated next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,15 +1,6 @@
-<<<<<<< Updated upstream
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 /// <reference path="./.next/types/routes.d.ts" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
-=======
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-/// <reference path="./.next/types/routes.d.ts" />
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
->>>>>>> Stashed changes
